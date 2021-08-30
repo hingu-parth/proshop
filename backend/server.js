54993a6307431aa1025dc7085f64f862bd79b498
Parth Hingu <parthhingu@parths-mbp.myfiosgateway.com>
2021-08-30 14:13:11 -0400

    Fetching from databse

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import colors from 'colors';
 import products from './data/products.js';
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
 import dotenv from 'dotenv';
 dotenv.config();
 connectDB();
@@ -12,14 +13,7 @@ app.get('/', (req, res) => {
   res.send('API working...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((product) => product._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
