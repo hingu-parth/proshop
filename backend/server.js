6c2f7d00b86905ba42151918cafc376382374722
Parth Hingu <parthhingu@parths-mbp.myfiosgateway.com>
2021-08-30 15:04:13 -0400

    Created custom error handlers

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import colors from 'colors';
 import products from './data/products.js';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import dotenv from 'dotenv';
 dotenv.config();
 connectDB();
@@ -14,6 +15,10 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
