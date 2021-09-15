689aefbe74ff70696794479cbcfe251b035caa9d
Parth Hingu <parthhingu@parths-mbp.myfiosgateway.com>
2021-09-15 15:42:52 -0400

    Bug fixes

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -79,6 +79,8 @@ export const userUpdateProfileReducer = (state = {}, action) => {
       return { loading: false, success: true, user: action.payload };
     case USER_UPDATE_PROFILE_FAIL:
       return { loading: false, error: action.payload };
+    case USER_UPDATE_PROFILE_RESET:
+      return {};
     default:
       return state;
   }
