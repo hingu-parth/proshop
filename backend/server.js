bdf3dee4d225fd82864f2e7930016702d12166b3
Parth Hingu <parthhingu@parths-mbp.myfiosgateway.com>
2021-09-15 15:08:02 -0400

    Last steps

--- backend/server.js
+++ backend/server.js
@@ -15,15 +15,11 @@ connectDB();
 
 const app = express();
 
-if (process.env.NODE_ENV === 'Development Mode') {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('API working...');
-});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -36,6 +32,16 @@ app.get('/api/config/paypal', (req, res) => {
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API working...');
+  });
+}
 
 app.use(notFound);
 
